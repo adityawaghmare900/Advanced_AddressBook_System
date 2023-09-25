aeaeee1089a7aa2331d80e17fdf43c4156683213
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-25 21:40:14 +0530

    [aditya]: create a addres book service DB

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CreateDataBase_UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            CustomerOperation.createDB();
+        }
+    }
+}
